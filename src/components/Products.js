0157060271cbe2156f9800afd4d21a0dee6273a7
simran <bhalla.simran297@gmail.com>
2021-01-02 17:38:24 +0530

    done

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,21 +2,17 @@ import React from "react";
 import imageproductive from "../images/illustration-stay-productive.png";
 function Products() {
   return (
-    <div className="productive_container">
-      <div
-        className="productive container"
-        style={{ marginBottom: "0", marginTop: "0" }}
-      >
-        <div className="productive-img" data-aos="fade-right">
+    <div className="productive-container">
+      <div className="productive container">
+        <div className="productive-img">
           <img src={imageproductive} alt={imageproductive} />
         </div>
-        <div className="productive-txt" data-aos="fade-left">
+        <div className="productive-txt">
           <h1>Stay productive, wherever you are</h1>
           <p>
             Never let location be an issue when accessing your files. Fylo has
             you covered for all of your file storage needs.
           </p>
-
           <p>
             Securely share files and folders with friends, family and colleagues
             for live collaboration. No email attachments required.
